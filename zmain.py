0db61486887ee28362063475d9cdc38a932e931a
carolnatlz <60240726+carolnatlz@users.noreply.github.com>
2022-04-10 18:17:22 -0300

    Revert "first commit"
    This reverts commit 2fdb0dfa67bcb58b81712952da8baf7a5e4c43d2.

--- zmain.py
+++
@@ -1,6 +0,0 @@
-#o init é o arquivo de inicialização, caso precisasse de outro bastava escrever: from siteprojeto.nomedoarquivo
- 
-from siteprojeto import app
-
-if __name__ == '__zmain__':
-    app.run(debug=True)
